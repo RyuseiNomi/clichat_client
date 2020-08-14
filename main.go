6507af648dc88584937298f2ca917f71993448b4
RyuseiNomi <kanontonto1010@gmail.com>
2020-08-14 14:01:04 +0900

    add a process to send a message as byte to go_server

--- main.go
+++ main.go
@@ -22,22 +22,25 @@ func main() {
 		log.Fatal("Dial: ", err)
 	}
 	for {
-		input := getStdin()
-		if input == "exit" {
+		stringInput, byteInput := getStdin()
+		if stringInput == "exit" {
 			c.Close()
 			break
 		}
+		c.WriteMessage(websocket.TextMessage, byteInput)
 	}
 }
 
 // getStdin ターミナルからの標準入力を受け取る
 // "exit"を入力された場合にプログラムを終了する
-func getStdin() string {
+func getStdin() (string, []byte) {
 	scanner := bufio.NewScanner(os.Stdin)
-
 	scanner.Scan()
-	stringInput := scanner.Text()
 
-	stringInput = strings.TrimSpace(stringInput)
-	return stringInput
+	// stringはmain()で"exit"の判定を行うため。終了判定方法を変更した場合これは不要になる
+	// byteはサーバへのメッセージ送信に利用するため
+	stringInput := strings.TrimSpace(scanner.Text())
+	byteInput := scanner.Bytes()
+
+	return stringInput, byteInput
 }
